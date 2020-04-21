78c02e77e1dd45f8b429c4237bcd1480cb9c3cbf
MichalBogoryja <m.michal.bz@gmail.com>
2020-04-21 19:51:47 +0200

    Add possibility to check longer texts than one word

--- Modul_04/Task_4_2.py
+++ Modul_04/Task_4_2.py
@@ -1,20 +1,23 @@
-def check_palindromes(word: str) -> bool:
+def check_palindromes(text: str) -> bool:
     """
-    Returns 'True' or 'False', based the argument being palindrome or not:
+    Returns 'True' or 'False', based on the argument being palindrome or not:
     Argument:
-        word
+        text
     """
 
-    text_length = len(word)
-    if text_length % 2 == 0:
-        print(1)
-        return False
+    text = text.replace(' ', '').lower()
+    text_length = len(text)
 
-    for i in range(int((text_length - 1) / 2)):
-        if word[i] != word[-(i+1)]:
-            return False
+    if text_length % 2 == 0:
+        for i in range(int(text_length / 2)):
+            if text[i] != text[-(i + 1)]:
+                return False
+    else:
+        for i in range(int((text_length - 1) / 2)):
+            if text[i] != text[-(i + 1)]:
+                return False
 
     return True
 
 
-print(check_palindromes("kajak"))
+print(check_palindromes("Akta generała ma mała renegatka"))
