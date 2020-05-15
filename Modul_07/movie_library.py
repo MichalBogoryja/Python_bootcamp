2c05412a0135f6eaddc7006234f8e067cffd9927
MichalBogoryja <m.michal.bz@gmail.com>
2020-05-15 13:59:57 +0200

    Add function to generate movies and series

--- Modul_07/movie_library.py
+++ Modul_07/movie_library.py
@@ -1,3 +1,7 @@
+import requests
+import json
+import random
+
 class Movie:
     def __init__(self, title, year, genre):
         self.title = title
@@ -15,6 +19,18 @@ Number of views: {self.view_count}
 '''
         return text
 
+    def __repr__(self):
+        text = f'''
+
+Title: {self.title}
+Year: {self.year}
+Genre: {self.genre}
+Number of views: {self.view_count}'''
+        return text
+
+    def play(self):
+        self.view_count += 1
+
 
 class Series(Movie):
     def __init__(self, season, episode, *args, **kwargs):
@@ -23,20 +39,50 @@ class Series(Movie):
         self.episode = episode
 
     def __str__(self):
-        return f'''{super().__str__()} \
-Season: {self.season}
-Episode: {self.episode}
+        return f'''{super().__str__()}
+S{self.season:02}E{self.episode:02}
+'''
+
+    def __repr__(self):
+        return f'''{super().__repr__()}
+S{self.season.rjust(2, "0")}E{self.episode:02}
 '''
 
 
-def play(film):
-    film.view_count += 1
+def get_film_data(title):
+    response = requests.get(f'http://www.omdbapi.com/?apikey=74a6ab0e&'
+                            f't={title}')
+    film_data = json.loads(response.text)
+    if film_data['Response'] == 'False':
+        film_data = False
+    print(film_data)
+
+    return film_data
 
 
-shrek = Movie("Shrek", "2008", "Kids")
-twin_peaks = Series(title="Twin Peaks", year="1990", genre="Drama", season="1", episode="04")
-play(shrek)
-play(twin_peaks)
+def generate_one_series():
+    pass
 
-print(shrek)
-print(twin_peaks)
+
+def generate_film(number):
+    film = []
+    m = open('movie_list.txt')
+    s = open('series_list.txt')
+    for i in range(number):
+        check = random.randint(0, 1)
+        if check == 1:
+            title = m.readline().replace('\n', '')
+        else:
+            title = s.readline().replace('\n', '')
+        film_data = get_film_data(title)
+        if not film_data:
+            film.append(f'\nMovie {title} doesnt exist')
+        elif film_data['Type'] == 'movie':
+            film.append(Movie(title, film_data['Year'], film_data['Genre']))
+        else:
+            film.append(Series(title=title,
+                               year=film_data['Year'],
+                               genre=film_data['Genre'],
+                               season=film_data['totalSeasons'],
+                               episode=1))
+    return film
---
+++ Modul_07/movie_library_cli.py
@@ -0,0 +1,5 @@
+from movie_library import generate_film
+
+library = generate_film(5)
+
+print(library)
