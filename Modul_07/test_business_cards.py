40a75fba750bda419dfca3c4c88ee9ddda217044
MichalBogoryja <m.michal.bz@gmail.com>
2020-05-18 11:47:51 +0200

    Add testing mails structure

--- Modul_07/test_business_cards.py
+++ Modul_07/test_business_cards.py
@@ -1,7 +1,13 @@
 from business_cards import create_contacts
 
 
-def test_phone():
+def test_phone_number_has_correct_length():
     card = create_contacts(1, True)
     phone_number = card[0].phone.replace(" ", "")
     assert 9 <= len(phone_number) <= 12
+
+
+def test_mail_structure():
+    card = create_contacts(1, True)
+    mail = card[0].mail
+    assert mail.replace(".", "").replace("@", "").isalnum()
